e1df630676e326373fb53e6e8cddcc03269c0c5f
KrisztianToth <krisztian.toth@labela.nl>
2018-12-10 14:01:49 -0800

    remove README init from setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
-import os
-
 from setuptools import setup, find_packages
-
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
-    README = f.read()
 
 requires = [
     'plaster_pastedeploy==0.6.*',
@@ -26,7 +20,6 @@ setup(
     name='hackathon_la',
     version='0.0',
     description='hackathon_la-iq',
-    long_description=README,
     classifiers=[
         'Programming Language :: Python',
         'Framework :: Pyramid',
