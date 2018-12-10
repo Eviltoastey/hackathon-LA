5ec3d81d17a7416bfc47cbba2764a97645e5c05d
KrisztianToth <krisztian.toth@labela.nl>
2018-12-10 14:01:16 -0800

    init commit

---
+++ hackathon_la/model/__init__.py
@@ -0,0 +1,53 @@
+import zope.sqlalchemy
+
+from sqlalchemy import engine_from_config
+from sqlalchemy.orm import sessionmaker, configure_mappers
+
+# import or define all models here to ensure they are attached to the
+# Base.metadata prior to any initialization routines
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.schema import MetaData
+
+# Recommended naming convention used by Alembic, as various different database
+# providers will autogenerate vastly different names making migrations more
+# difficult. See: http://alembic.zzzcomputing.com/en/latest/naming.html
+NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=NAMING_CONVENTION)
+Base = declarative_base(metadata=metadata)
+
+# run configure_mappers after defining all of the models to ensure
+# all relationships can be setup
+configure_mappers()
+
+
+def get_engine(settings, prefix='sqlalchemy_'):
+    return engine_from_config(settings, prefix)
+
+
+def get_session_factory(engine):
+    factory = sessionmaker()
+    factory.configure(bind=engine)
+    return factory
+
+
+def get_tm_session(session_factory, transaction_manager):
+    dbsession = session_factory()
+    zope.sqlalchemy.register(
+        dbsession, transaction_manager=transaction_manager)
+    return dbsession
+
+
+def includeme(config):
+    """
+    Add the Pyramid transaction manager to the processing of each request.
+    :param config: the `Configurator` object passed by the Pyramid framework.
+    """
+    config.include('pyramid_tm')
---
+++ setup.py
@@ -0,0 +1,52 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+
+requires = [
+    'plaster_pastedeploy==0.6.*',
+    'pyramid==1.9.*',
+    'pyramid_debugtoolbar==4.5.*',
+    'waitress==1.1.*',
+    'SQLAlchemy==1.2.*',
+    'zope.sqlalchemy==1.0.*',
+    'dependency_injector==3.14.*'
+]
+
+tests_require = [
+    'WebTest==2.0.*',
+    'pytest==3.9.*',
+    'pytest-cov==2.6.*',
+]
+
+setup(
+    name='hackathon_la',
+    version='0.0',
+    description='hackathon_la-iq',
+    long_description=README,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web pyramid pylons',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'testing': tests_require,
+    },
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = bundelz_iq:main',
+        ],
+    },
+)
